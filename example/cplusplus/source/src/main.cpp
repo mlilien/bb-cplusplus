7fce8f93e73a2a029d7205f370e00bbafd4ff953
Jörg Zeidler <joerg.zeidler@conplement.de>
2020-06-17 16:28:06 +0200

    #8: use version cpp header file in main.cpp example

--- example/cplusplus/source/src/main.cpp
+++ example/cplusplus/source/src/main.cpp
@@ -1,12 +1,14 @@
 #include <chrono>
 #include <iostream>
 #include <thread>
+#include "version.h"
 
 int main(int argc, char* argv[])
 {
     while (true)
     {
         std::cout << "Hello world! Waiting to be terminated." << std::endl;
+        std::cout << "InformationalVersion: " << version::InformationalVersion << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(5));
     }
     return 0;
