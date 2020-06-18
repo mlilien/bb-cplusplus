5d1da8cf36a60fde3ebdc7c23c9e174f3aea888c
Armin Schlegel <armin.schlegel@conplement.de>
2020-06-18 11:57:12 +0200

    #8: version header namespace elbb::version
    added additional FullSemVer output

--- example/cplusplus/source/src/main.cpp
+++ example/cplusplus/source/src/main.cpp
@@ -1,15 +1,16 @@
 #include <chrono>
 #include <iostream>
 #include <thread>
-#include <version.h>
 
-int main(int argc, char* argv[])
-{
-    while (true)
-    {
-        std::cout << "Hello world! Waiting to be terminated." << std::endl;
-        std::cout << "InformationalVersion: " << version::InformationalVersion << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(5));
-    }
-    return 0;
+#include <elbb/version.h>
+
+int main(int argc, char *argv[]) {
+  while (true) {
+    std::cout << "Hello world! Waiting to be terminated." << std::endl;
+    std::cout << "FullSemVer: " << elbb::version::FullSemVer << std::endl;
+    std::cout << "InformationalVersion: " << elbb::version::InformationalVersion
+              << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+  }
+  return 0;
 }
