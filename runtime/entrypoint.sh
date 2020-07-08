f7aa1a7ce309e3771b2df397d991395bbb364ea5
Marcel Lilienthal <134974+mlilien@users.noreply.github.com>
2020-07-08 17:52:17 +0200

    elbb/bb-cplusplus#20 trap and forward signals in runtime entrypoint

--- runtime/entrypoint.sh
+++ runtime/entrypoint.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-exec "$@"
+_int()
+{
+    echo "Catched SIGINT"
+    kill -INT ${pid}
+}
+
+_term()
+{
+    echo "Catched SIGTERM"
+    killall -TERM ${pid}
+}
+
+trap _term SIGTERM
+trap _int SIGINT
+
+${1} &
+pid=$!
+
+wait -n ${pid}
+exit $?
