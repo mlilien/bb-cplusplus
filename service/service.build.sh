c95df47b969a08667f8460a38d9c18ef54e5edc1
Marcel Lilienthal <134974+mlilien@users.noreply.github.com>
2020-09-23 10:38:38 +0200

    add armv7hf support

--- service/service.build.sh
+++ service/service.build.sh
@@ -19,12 +19,20 @@ echo SOURCE_DIR=${SOURCE_DIR}
 if [ -z ${CONAN_REMOTE} ]; then CONAN_REMOTE=http://artifactory-cpp-ce:8082/artifactory/api/conan/conan-local; fi
 conan remote add conan-local ${CONAN_REMOTE} ${CONAN_SSL_VERIFICATION} -f
 
+if [ "${ARCH}" = armv7hf ]; then
+    CMAKE_CXX_COMPILER=/usr/bin/arm-linux-gnueabihf-g++
+    STRIP_CMD=/usr/bin/arm-linux-gnueabihf-strip
+else
+    CMAKE_CXX_COMPILER=/usr/bin/${ARCH}-linux-gnu-g++
+    STRIP_CMD=${ARCH}-linux-gnu-strip
+fi
+
 mkdir -p ${BUILD_DIR} \
 && cd ${BUILD_DIR} \
 && ( conan install ${SOURCE_DIR} --profile=/conan.${ARCH}.profile --build missing -r conan-local || (conan install ${SOURCE_DIR} --profile=/conan.${ARCH}.profile --build missing ) \
 && conan upload "*" -r conan-local --all -c; )
 
-cmake ${SOURCE_DIR} -DCMAKE_CXX_COMPILER=/usr/bin/${ARCH}-linux-gnu-g++ -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}  \
+cmake ${SOURCE_DIR} -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}  \
 && make -j${CONAN_CPU_COUNT} \
-&& if [ ${CMAKE_BUILD_TYPE,,} == "release" ]; then ${ARCH}-linux-gnu-strip cplusplus_service; fi \
+&& if [ ${CMAKE_BUILD_TYPE,,} == "release" ]; then ${STRIP_CMD} cplusplus_service; fi \
 && make install;
